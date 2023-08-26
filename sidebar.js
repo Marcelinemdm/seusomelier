22299d00ad2a9469fa53c4dc79c0e18eb2ca0096
seusomelier <fefetodobom@hotmail.com>
2023-08-26 16:37:58 -0300

    commit

---
+++ sidebar.js
@@ -0,0 +1,7 @@
+document.getElementById("openSidebar").addEventListener("click", function() {
+  document.getElementById("mySidebar").style.width = "250px";
+});
+
+document.getElementById("closeSidebar").addEventListener("click", function() {
+  document.getElementById("mySidebar").style.width = "0";
+});
