89b0b4650cdb0f328f5c0d1c4cee2c7fdc9afc3b
seusomelier <fefetodobom@hotmail.com>
2024-01-02 18:33:53 -0300

    commit

---
+++ about-me/script.js
@@ -0,0 +1,34 @@
+//currentYearMonth
+document.addEventListener('DOMContentLoaded', function () {
+  const currentYearMonthElement = document.getElementById('currentYearMonth');
+  if (currentYearMonthElement) {
+      const currentDate = new Date();
+      const currentYear = currentDate.getFullYear();
+      const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
+
+      currentYearMonthElement.textContent = `${currentMonth} ${currentYear} SeuSomelier `;
+  }
+});
+
+//currentAge
+document.addEventListener('DOMContentLoaded', function () {
+  const currentAgeElement = document.getElementById('currentAge');
+  if (currentAgeElement) {
+      const currentDate = new Date();
+      const birthDate = new Date('2003-08-28');
+      let age = calculateAge(birthDate, currentDate);
+
+      if (currentDate.getMonth() > birthDate.getMonth() ||
+          (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() >= birthDate.getDate())) {
+          age++;
+      }
+
+      currentAgeElement.innerHTML = `Working as a full stack developer<br>${age} years old`;
+  }
+});
+
+function calculateAge(birthDate, currentDate) {
+  const diffInMilliseconds = currentDate - birthDate;
+  const ageDate = new Date(diffInMilliseconds);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
