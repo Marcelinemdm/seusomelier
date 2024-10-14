5b98b030aa66f6e09267113920d66ae8a90ff21a
seusomelier <89878440+Marcelinemdm@users.noreply.github.com>
2024-10-13 22:04:19 -0300

    commit

--- bio/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  setTimeout(function () {
-    document.getElementById('popupOverlay').style.display = 'flex';
-  }, 2000);
-});
-
-function closePopup() {
-  document.getElementById('popupOverlay').style.display = 'none';
-}
