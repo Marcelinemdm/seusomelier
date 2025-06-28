96bb9da04f114f5a3c5788fdccee319f339424ac
seusomelier <89878440+Marcelinemdm@users.noreply.github.com>
2025-06-28 00:04:32 -0300

    commir

--- sidebar.js
+++
@@ -1,7 +0,0 @@
-// document.getElementById("openSidebar").addEventListener("click", function() {
-//   document.getElementById("mySidebar").style.width = "250px";
-// });
-
-// document.getElementById("closeSidebar").addEventListener("click", function() {
-//   document.getElementById("mySidebar").style.width = "0";
-// });
